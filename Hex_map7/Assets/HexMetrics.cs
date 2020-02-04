85eb1af03ce69f9f0f83fa6ac6fa551f6860673d
ming <isyiming.zhang@outlook.com>
2020-02-04 19:27:22 +0800

    添加了道路

---
+++ Hex_map7/Assets/HexMetrics.cs
@@ -0,0 +1,139 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class HexMetrics
+{
+    //单元格高度梯度
+    public const float elevationStep = 3f;
+
+    //定义单元格有几个梯度
+    public const int terracesPerSlope = 3;
+    public const int terraceSteps = terracesPerSlope * 2 + 1;
+
+    public const float outerToInner = 0.866025404f;
+    public const float innerToOuter = 1f / outerToInner;
+
+    //定义六边形的外接圆和内切圆半径
+    public const float outerRadius = 10f;
+    public const float innerRadius = outerRadius * 0.866025404f;
+
+    //定义六边形六个角相对于中心的坐标
+    public static Vector3[] corners = {
+        new Vector3(0f, 0f, outerRadius),
+        new Vector3(innerRadius, 0f, 0.5f * outerRadius),
+        new Vector3(innerRadius, 0f, -0.5f * outerRadius),
+        new Vector3(0f, 0f, -outerRadius),
+        new Vector3(-innerRadius, 0f, -0.5f * outerRadius),
+        new Vector3(-innerRadius, 0f, 0.5f * outerRadius),
+        new Vector3(0f, 0f, outerRadius)
+    };
+
+    //邻近单元格颜色混合时的混合系数
+    public const float solidFactor = 0.8f;
+    public const float blendFactor = 1f - solidFactor;
+
+
+    //噪声
+    public static Texture2D noiseSource;
+    public const float cellPerturbStrength = 4f;
+    public const float noiseScale = 0.003f;
+    public const float elevationPerturbStrength = 1.5f;
+
+    public const int chunkSizeX = 5, chunkSizeZ = 5;
+
+    //河床高程
+    public const float streamBedElevationOffset = -1.75f;
+
+    //确保水面不超过河床
+    public const float riverSurfaceElevationOffset = -0.5f;
+
+
+    public static Vector3 GetFirstCorner(HexDirection direction)
+    {
+        return corners[(int)direction];
+    }
+
+    public static Vector3 GetSecondCorner(HexDirection direction)
+    {
+        return corners[(int)direction + 1];
+    }
+
+    public static Vector3 GetFirstSolidCorner(HexDirection direction)
+    {
+        return corners[(int)direction] * solidFactor;
+    }
+
+    public static Vector3 GetSecondSolidCorner(HexDirection direction)
+    {
+        return corners[(int)direction + 1] * solidFactor;
+    }
+
+    public static Vector3 GetBridge(HexDirection direction)
+    {
+        return (corners[(int)direction] + corners[(int)direction + 1]) *
+            blendFactor;
+    }
+
+    //高度梯度变化，分阶段
+    public const float horizontalTerraceStepSize = 1f / terraceSteps;
+    public const float verticalTerraceStepSize = 1f / (terracesPerSlope + 1);
+
+    public static Vector3 TerraceLerp(Vector3 a, Vector3 b, int step)
+    {
+        float h = step * HexMetrics.horizontalTerraceStepSize;
+        a.x += (b.x - a.x) * h;
+        a.z += (b.z - a.z) * h;
+        float v = ((step + 1) / 2) * HexMetrics.verticalTerraceStepSize;
+        a.y += (b.y - a.y) * v;
+        return a;
+    }
+
+    //为颜色梯度变化提供方法
+    public static Color TerraceLerp(Color a, Color b, int step)
+    {
+        float h = step * HexMetrics.horizontalTerraceStepSize;
+        return Color.Lerp(a, b, h);
+    }
+
+
+
+    public static HexEdgeType GetEdgeType(int elevation1, int elevation2)
+    {
+        if (elevation1 == elevation2)
+        {
+            return HexEdgeType.Flat;
+        }
+        int delta = elevation2 - elevation1;
+        if (delta == 1 || delta == -1)
+        {
+            return HexEdgeType.Slope;
+        }
+        return HexEdgeType.Cliff;
+    }
+
+    public static Vector4 SampleNoise(Vector3 position)
+    {
+        return noiseSource.GetPixelBilinear(
+            position.x * noiseScale,
+            position.z * noiseScale
+        );
+    }
+
+
+    public static Vector3 GetSolidEdgeMiddle(HexDirection direction)
+    {
+        return
+            (corners[(int)direction] + corners[(int)direction + 1]) *
+            (0.5f * solidFactor);
+    }
+
+
+    public static Vector3 Perturb(Vector3 position)
+    {
+        Vector4 sample = SampleNoise(position);
+        position.x += (sample.x * 2f - 1f) * cellPerturbStrength;
+        position.z += (sample.z * 2f - 1f) * cellPerturbStrength;
+        return position;
+    }
+    }
